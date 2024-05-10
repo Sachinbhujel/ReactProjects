27ef5359cc2e8d9a5255f09fd665c0306cbf5ede
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-05-10 19:24:42 +0000

    Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,32 @@
-import './App.css'
-import data from './Data'
+import React, { useState } from 'react';
+import './App.css';
+import data from './Data';
+
 function App() {
-alert(data)
+  let [items, setItems] = useState(data);
+
+  const removeItem = (index) => {
+    const newList = items.filter((_, i) => i !== index);
+    setItems(newList);
+  };
+  
   return (
-    <>
-      <div class="main">
-        <h3>Title</h3>
-        <p>Mesaage</p>
-        <h1>X</h1>
+    <div className="container">
+      <div className="main">
+        {items.map((item, index) => {
+          let borderColor = item.border === 'green' ? 'green' : 'red';
+
+          return (
+            <div className="item" key={index} style={{ border: `4px solid ${borderColor}` }}>
+              <h3>{item.title}</h3>
+              <p>{item.message}</p>
+              <h1 className='cross' onClick={() => removeItem(index)}>X</h1>
+            </div>
+          );
+        })}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/Data.jsx
+++ vite-project/src/Data.jsx
@@ -1,3 +1,3 @@
-let data = [{title: "Succes", message: "Success"}, {title: "warning", message: "warning"}];
+let data = [{title: "Succes", message: "Successfully Done!!!", border: "green"}, {title: "Warning", message: "Warning Don't Come Here!!!", border: "red"}];
 
 export default data;
