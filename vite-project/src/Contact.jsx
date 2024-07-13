b1968aa129b1399dec17a8b3f8d690add7bc473d
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-07-13 16:44:41 +0000

    save

--- vite-project/src/Contact.jsx
+++ vite-project/src/Contact.jsx
@@ -1,11 +1,26 @@
+import React, { useState , useRef} from 'react';
 import './Contact.css';
 
 function Contact() {
+  const [statusMessage, setStatusMessage] = useState('');
+  const formRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    setStatusMessage('Your message has been sent!');
+
+    
+    setTimeout(() => {
+      setStatusMessage('');
+    }, 2000);
+
+    formRef.current.reset();
+  };
+
   return (
     <>
       <div className="contact-form-container">
         <h1>Contact Us</h1>
-        <form id="contact-form">
+        <form id="contact-form" onSubmit={handleSubmit} ref={formRef}>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" required />
 
@@ -17,7 +32,7 @@ function Contact() {
 
           <button type="submit">Send</button>
         </form>
-        <p id="status-message"></p>
+        <p id="status-message">{statusMessage}</p>
       </div>
     </>
   );
