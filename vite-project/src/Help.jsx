7ee77b68760f2947abdc8b7cb01f1a730f43df8f
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-07-19 21:38:18 +0530

    Update Help.jsx

--- vite-project/src/Help.jsx
+++ vite-project/src/Help.jsx
@@ -4,16 +4,28 @@ import React,{useState} from 'react';
 function Help() {
   const [isActive, setIsActive] = useState(false);
 
-  let data = [{
-    title: 'section 1',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-    laborum cupiditate possimus labore, hic temporibus velit dicta earum
-    suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-    voluptatem.`,
-  }, {
-    title: 'section 2',
-    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapient'
-  }];
+  let data = [   {
+      title: 'Section 1',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+      laborum cupiditate possimus labore, hic temporibus velit dicta earum
+      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+      voluptatem.`
+    },
+    {
+      title: 'Section 2',
+      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
+      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
+      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
+      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
+      Repudiandae, mollitia id reprehenderit a ab odit!`
+    },
+    {
+      title: 'Section 3',
+      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
+      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
+      dolor ut sequi minus iste? Quas?`
+    }];
 
   return (
     <>
