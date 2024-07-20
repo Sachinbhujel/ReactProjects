77449857fe9e15f6721282bfcd0d8c9cdb23666d
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-07-20 09:55:54 +0000

    Changes

--- vite-project/src/Help.jsx
+++ vite-project/src/Help.jsx
@@ -1,18 +1,19 @@
-import React,{useState} from 'react';
-
+import React, { useState } from 'react';
+import './Help.css';
 
 function Help() {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(-1);
 
-  let data = [   {
-      title: 'Section 1',
+  const data = [
+    {
+      title: 'Question 1',
       content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
       laborum cupiditate possimus labore, hic temporibus velit dicta earum
       suscipit commodi eum enim atque at? Et perspiciatis dolore iure
       voluptatem.`
     },
     {
-      title: 'Section 2',
+      title: 'Question 2',
       content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
       reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
       quaerat iure quos dolorum accusantium ducimus in illum vero commodi
@@ -21,29 +22,31 @@ function Help() {
       Repudiandae, mollitia id reprehenderit a ab odit!`
     },
     {
-      title: 'Section 3',
+      title: 'Question 3',
       content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
       quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
       dolor ut sequi minus iste? Quas?`
-    }];
+    }
+  ];
 
   return (
-    <>
-      <h1>FAQ</h1>
+    <div className="faq_item">
       {data.map((item, index) => (
-        <div className="faq" key={index}>
-        <div className="faq_item">
-          <div className="faq_title" onClick={() => setIsActive(!isActive)}> 
+        <div key={index}>
+          <div className="faq_title" onClick={() => setIsActive(isActive === index ? -1 : index)}>
             <div>{item.title}</div>
-            <div>{isActive ? '-' : '+'}</div>
+            <div>
+              {isActive === index ? (
+                <span className="material-symbols-outlined">remove</span>
+              ) : (
+                <span className="material-symbols-outlined">add</span>
+              )}
+            </div>
           </div>
-          <div className="faq_content">
-          {isActive && <div className="accordion-content">{item.content}</div>}
-          </div>
+          {isActive === index && <div className="faq_content">{item.content}</div>}
         </div>
-      </div>
       ))}
-    </>
+    </div>
   );
 }
 
